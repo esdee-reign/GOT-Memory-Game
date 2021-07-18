6a42adc6e3ce16c1a91e881fe544f50f0d1ab92f
unknown <snehdeepsingh221199@gmail.com>
2021-07-18 13:22:57 +0530

    overall styling fixed

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect } from 'react';
-import './GameBoard.css';
 import HouseCard from '../HouseCard';
 import houseList from '../houseList.json';
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
 
 const Nav = (props) => {
     return(
-        <Navbar variant="dark" >
+        <Navbar variant="dark" style={{backgroundColor: "black", border: "1px solid white"}}>
             <Container>
                 <Navbar.Brand href="#">
                     <div nav-container>
-                        <div className="title">Game Of Thrones Houses - Memory Game</div>
-                        <div className="score">
-                            Current Score: {props.score}
-                            High Score: {props.bestScore}
-                        </div>
+                        <Row>
+                        <Col className="title">Game Of Thrones &nbsp; - &nbsp; Memory Game</Col>
+                        <Col className="score">Current Score : &nbsp; {props.score} &nbsp; High Score : &nbsp; {props.bestScore}</Col>
+                        </Row>
                     </div>
                 </Navbar.Brand>
             </Container>
