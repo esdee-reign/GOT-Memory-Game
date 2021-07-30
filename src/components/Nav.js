a16bbb56d2ad9f4524e68f5852b1b3d13b3c1f72
unknown <snehdeepsingh221199@gmail.com>
2021-07-30 10:23:15 +0530

    tar-url fix, css fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,11 @@ import { Navbar, Container, Row, Col } from 'react-bootstrap';
 
 const Nav = (props) => {
     return(
-        <Navbar variant="dark" style={{backgroundColor: "black", border: "1px solid white"}}>
+        <div  style={{backgroundColor: "black", border: "1px solid white", height: '68px'}}>
+        <Navbar variant="dark" >
             <Container>
                 <Navbar.Brand href="#">
-                    <div nav-container>
+                    <div>
                         <Row>
                         <Col className="title">Game Of Thrones &nbsp; - &nbsp; Memory Game</Col>
                         <Col className="score">Current Score : &nbsp; {props.score} &nbsp; High Score : &nbsp; {props.bestScore}</Col>
@@ -15,6 +16,7 @@ const Nav = (props) => {
                 </Navbar.Brand>
             </Container>
         </Navbar>
+        </div>
     )
 }
 
